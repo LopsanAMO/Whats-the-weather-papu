56c3280fb5319134c4950f95c559907af73a1be4
LopsanAMO <makiki789@gmail.com>
2016-09-24 06:35:23 -0500

    whats the weather app

---
+++ Whats the weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whats the weather
+//
+//  Created by Lopsan antonio Molina Osoio on 24/09/16.
+//  Copyright © 2016 OMLODI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
